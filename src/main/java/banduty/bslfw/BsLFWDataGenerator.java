fa96289ea2939555fa7e43840a10393433942ee2
B4nduty <bandutysmods@gmail.com>
2024-04-27 17:10:57 +0200

    Initial commit

---
+++ src/main/java/banduty/bslfw/BsLFWDataGenerator.java
@@ -0,0 +1,11 @@
+package banduty.bslfw;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BsLFWDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
