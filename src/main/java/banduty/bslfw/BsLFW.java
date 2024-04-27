625d5c110d8816ec51b5605617fe649b5599d0b3
B4nduty <bandutysmods@gmail.com>
2024-04-27 17:15:34 +0200

    Version 0.0.2

--- src/main/java/banduty/bslfw/BsLFW.java
+++ src/main/java/banduty/bslfw/BsLFW.java
@@ -1,22 +1,26 @@
 package banduty.bslfw;
 
+import banduty.bslfw.config.ModConfigs;
+import banduty.bslfw.item.ModItems;
+import banduty.bslfw.util.LootTableModifier;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+import me.shedaniel.autoconfig.serializer.PartitioningSerializer;
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BsLFW implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("bslfw");
+	public static final String MOD_ID = "bslfw";
+	public static final Logger LOGGER = LoggerFactory.getLogger("bslfw");
+	public static ModConfigs CONFIG;
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		AutoConfig.register(ModConfigs.class, PartitioningSerializer.wrap(JanksonConfigSerializer::new));
+		CONFIG = AutoConfig.getConfigHolder(ModConfigs.class).getConfig();
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+		LootTableModifier.modifyLootTables();
 	}
 }
